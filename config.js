c0168562a61dd0f739840f4c07d77925b7a04447
alexischan <alexreyd44@gmail.com>
2026-07-14 20:30:04 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+export default {
+    nombre: '`𓆩♡𓆪 𝕄𝕚𝕜𝕦 𓆩♡𓆪`',
+    nombre2: '`✧･ﾟ: _𝙼𝚒𝚔𝚞 𝙽𝚊𝚔𝚊𝚗𝚘_:･ﾟ✧`',
+    prefix: '-',
+    banner: 'https://files.catbox.moe/qx3kea.jpeg',
+    botowner: '51970334698', '51924090815',
+    owner: ['51970334698', '51924090815'],
+    version: '1.0.4',
+    tipo: 'Principal',
+    creadorl: '卐ⁱᵃᵐ『Michigely_bam』ᴼᶠⁱᶜⁱᵃˡ',
+    canalId: '120363408963824114@newsletter',
+    canalNombre: '🄵🄾🄲💡 :3 | 𝙵𝙰𝙽𝙰𝚃𝙸𝙲𝙾𝚂 𝙳𝙴 𝚃𝙳𝙾 𝚄𝙽 𝙵𝙲𝙾                                                       Intento Nosé',
+};
